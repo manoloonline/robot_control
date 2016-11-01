5c6140b922c15c8b6f30f502b47d34ca74f2dd28
Mini <Mini@192.168.0.27>
2016-11-01 15:23:54 +0100

    first commit

---
+++ src/arduino_code/cocherobot/cocherobot.ino
@@ -0,0 +1,85 @@
+#include <Servo.h> // lamar a la libreria del servomotor
+
+Servo myservo;  // crear el objeto servo para controlar al servomotor
+int val=90;    // variable que almacenara los grados de movimiento del servomotor
+int led = 3;
+int led_verde = 4; //Un polo del motor
+int led_rojo = 5;  //Otro polo
+int motorPin = 6;  // Regulacion de velocidad
+
+void setup()
+{ 
+  pinMode(led, OUTPUT);
+  pinMode(led_verde, OUTPUT);
+  pinMode(led_rojo, OUTPUT);
+  pinMode(motorPin, OUTPUT);
+  digitalWrite(led_verde, HIGH);
+  digitalWrite(led_rojo, HIGH);
+  myservo.attach(9);  // atribuye el pin 9 al servomotor para transferir los datos
+  Serial.begin(9600); // iniciar comunicacion serial
+} 
+
+void loop()
+{ 
+  if (Serial.available() > 0) { //lee si el monitor serial recibe datos, si es asi entra al ciclo if
+    int inByte = Serial.read(); //asigna el contenido del monitor serial a la variable.
+
+    switch (inByte) {  //ciclos switch para determinar el valor que obtendra la variable val, segun sea el contenido de la variable inByte
+      case 48:  //el contenido delmonitor seral es recibido como codigo ASCII, donde cero  es igual a 48 en codigo ASCII
+        break;  //salir del ciclo
+      case '1': //la compracion tambien se puede hacer usando el caracter encerrado entre comillas simples 
+        digitalWrite(led, HIGH); //si el caso es 1 la variable val tendra el valor de 20
+        break;  //salir del ciclo
+      case '2': //...
+        digitalWrite(led, LOW);
+        break;
+      case '3':
+        val=140;
+        break;
+      case '4':
+        val=110;
+        break;
+      case '5':
+        val=90;
+        break;
+      case '6':
+        val=70;
+        break;
+      case '7':
+        val=40;
+        break;
+      case '8':
+        analogWrite(motorPin, 255);
+        break;
+      case '9':
+        analogWrite(motorPin, 180);
+        break;
+        case 'a':
+        analogWrite(motorPin, 135);
+        break;
+        case 'b':
+        analogWrite(motorPin, 100);
+        break;
+        case 'c':
+        analogWrite(motorPin, 0);
+        break;
+        case 'd': //Reversa
+        digitalWrite(led_verde, LOW);
+        digitalWrite(led_rojo, HIGH);
+        break;
+        case 'e': //Directa
+        digitalWrite(led_rojo, LOW);
+        digitalWrite(led_verde, HIGH);
+        break;
+        case 'f': //Directa
+        digitalWrite(led_rojo, HIGH);
+        digitalWrite(led_verde, HIGH);
+        break;
+      default:  // en el caso de que no se haya introducido algun valor entre 0 y 9 se imprimira un mensaje en el monitor serial
+        Serial.println("Introduce un valor entre 0-9 !!!");
+    }
+   myservo.write(val); 
+   delay(30);           // espera a que el servo llegue a la posicion indicada antes de recibir otra orden
+   }
+}
+
---
+++ src/pruebaarduino/PruebaArduino.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaarduino;
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamPanel;
+import java.awt.Dimension;
+import javax.swing.*;
+
+
+/**
+ *
+ * @author Manolo
+ */
+public class PruebaArduino {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*JPanel panel = new WebcamPanel(Webcam.getDefault());
+        JFrame window = new JFrame("Webcam capture");
+        Dimension d = new Dimension(400,300);
+        window.setPreferredSize(d);
+        window.add(panel);
+        window.pack();
+        window.setVisible(true);*/
+        
+        FramePrincipal aplicacion = new FramePrincipal("Robot");
+        Dimension x = new Dimension(400,150);
+        aplicacion.setPreferredSize(x);
+        aplicacion.setLocation(400, 0);
+        aplicacion.setFocusable(true);
+        aplicacion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        aplicacion.setVisible (true);
+        
+
+        
+    }
+}
